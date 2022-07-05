a2cbd76b8b5e574b24d6ca22a64d3f313161c5e6
ThankGod88 <enemuothankgod@yahoo.com>
2022-07-04 23:31:12 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    if my_list is None:
-        return ('None')
-    my_list.reverse()
-    for i in my_list:
-        print("{:d}".format(i))
+def no_c(my_string):
+    return my_string.translate({ord(c): None for c in "cC"})
